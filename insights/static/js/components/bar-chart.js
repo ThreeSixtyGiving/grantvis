1cf3190af2ddbe1d8604fe6c749bdec72811f533
Myles Lewando <myles.lewando@opendataservices.coop>
2023-08-04 17:04:30 +0100

    WIP

--- insights/static/js/components/bar-chart.js
+++ insights/static/js/components/bar-chart.js
@@ -1,137 +1,52 @@
 const MS_IN_DAY = (1000 * 60 * 60 * 24); // number of milliseconds in a day
 const date = new Date();
 let daysRange, lastYearLabel, language;
-let days = []
-if (window.navigator.languages) {
-  language = window.navigator.languages[0];
-} else {
-  language = window.navigator.userLanguage || window.navigator.language;
-}
 
-// Create empty dataset with 'Older' entry to be populated with this.chartData entries
-const compiledData = {
-  labels: ['Older'],
-  datasets: [{
-    backgroundColor: '#DE6E26',
-    hoverBackgroundColor: '#A8501A',
-    data: [0]
-  }]
-};
+let days = []
 
 // Check if date range is filtered
-let params = new URLSearchParams(document.location.search);
-let awardDates = false
-for (const [key, value] of params) {
-  if (key.includes('min_date')) {
-    awardDates = true;
-  }
-}
+
 
 export const barChart = {
-  extends: VueChartJs.Bar,
-  mixins: [VueChartJs.mixins.reactiveProp],
-  props: ['chartData', 'sourceData', 'hideLegend'],
+  name: 'BarChart',
+  props: {
+    chartData: {
+      type: Array,
+      required: true,
+    }
+  },
   data() {
-      return {
-
-      }
+    return {
+      compiledData: [{
+        key: 'Older',
+        doc_count: 0
+      }],
+      bars: null,
+      width: 600,
+      height: 300,
+      margin: { top: 20, right: 30, bottom: 40, left: 50 },
+    }
   },
   computed: {
-    options: function () {
-      console.log(this.chartData.datasets)
-
-      // this needs to be in the VueChartJs format or use raw data
-      this.sourceData.forEach((item, index) => {
+    processOlderData: function () {
+      this.compiledData[0].doc_count = 0;
+      this.chartData.forEach((item, index) => {
         const daysOld = Math.ceil((date - new Date(item.key)) / MS_IN_DAY);
         days.push(item.key)
         if (daysOld > 365 * 20) {
           // Push this.chartData older than 20 years into compiledData Older entry
-          compiledData.datasets[0].data[0] += Number(this.sourceData[index].doc_count);
+          this.compiledData[0].doc_count += this.chartData[index].doc_count;
         } else {
           // Insert all other data > 20 years old to compiledData dataset in reverse order
-            compiledData.labels.splice(1, 0, item.key);
-            compiledData.datasets[0].data.splice(1, 0, item.doc_count);
-          }
-        });
-
-      return {
-        responsive: true,
-        legend: {
-          display: (this.hideLegend ? false : true)
-        },
-        tooltips: { 
-          callbacks: {
-            title: function(tooltipItem, data) {
-              if (typeof tooltipItem[0].xLabel !== 'string') {
-                return new Date(tooltipItem[0].xLabel).toLocaleString(language, { year: 'numeric' });
-              } else {
-                return tooltipItem[0].xLabel
-              }
-            },
-            label: function(tooltipItem, data) {
-              return tooltipItem.yLabel.toLocaleString();
-            }
-          }
-        },
-        scales: {
-          xAxes: [{
-            gridLines: {
-              display: false,
-              offsetGridLines: true
-            },
-            offset: true,
-            ticks: {
-              autoSkip: false,
-              callback(value, index, ticks) {
-                // Parse and display date labels
-                if (typeof value !== 'string') {
-                  const month = new Date(value).toLocaleString(language, { month: 'numeric' });
-                  const year = new Date(value).toLocaleString(language, { year: 'numeric' });
-                  let label = null;
-
-                  daysRange = Math.ceil((Math.max(...days) - Math.min(...days)) / MS_IN_DAY)
-                  if (daysRange > 365 * 4) {
-                    // Hide date labels older than 20 years
-                    if (date.getFullYear() - year >= 20) {
-                      label = null;
-                    } else if (month === '10' || month === '11' || month === '12') {
-                      // Hide label if month is Oct / Nov / Dec to prevent overlapping
-                      label = null;
-                    } else if (year !== lastYearLabel) {
-                      // Show label on first instance of the year (prevents duplicates)
-                      label = year;
-                    } else {
-                      label = null;
-                    }
-                  } else {
-                    // Show month and year if date range < 1460 days (4 years)
-                    label = new Date(value).toLocaleString(language, { month: 'short', year: 'numeric' });
-                  }
-                  
-                  lastYearLabel = year;
-
-                  return label;
-                } else {
-                  // Show 'Older' (non-date) label if date range isn't filtered
-                  return value;
-                }
-              }
-            },
-            display: true,
-          }],
-          yAxes: [{
-            gridLines: {
-              display: false,
-            },
-            ticks: {
-              userCallback(value) {
-                return value.toLocaleString();
-              },
-                beginAtZero: true,
-                precision: 0
-            },
-          }]
+          this.compiledData.splice(1, 0, item);
         }
+      });
+
+      // Check if date range is filtered
+      if (this.isMinYearSet()) {
+        return this.chartData.reverse()
+      } else {
+        return this.compiledData
       }
     }
   },
@@ -139,7 +54,105 @@ export const barChart = {
     lastYearLabel = '';
   },
   mounted() {
-    // Switch dataset between original and 'Older' depending on if min date range filter
-    this.renderChart(compiledData, this.options)
-  }
-}
+    this.drawChart();
+    this.setTooltips();
+  },
+  watch: {
+    chartData: {
+      deep: true,
+      handler() {
+        this.isMinYearSet()
+        this.drawChart();
+        this.setTooltips();
+      },
+    },
+  },
+  methods: {
+    isMinYearSet () {
+      let params = new URLSearchParams(document.location.search);
+      let awardDates = false
+
+      for (const [key, value] of params) {
+        if (key.includes('min_date')) {
+          awardDates = true;
+        }
+      }
+      
+      return awardDates;
+    },
+    setTooltips(){
+      this.bars.attr('data-tippy-content', (d)=>{
+          return `<strong>${this.parseLabels(d.key)}</strong><br>${d.doc_count.toLocaleString()}`;
+      });
+      tippy(this.bars.nodes(), {
+        allowHTML: true
+      });
+    },
+    parseLabels (value) {
+      if (typeof value !== 'string') {
+        return new Date(value).toLocaleString(language, { year: 'numeric' });
+      } else {
+        return value;
+      }
+    },
+    drawChart() {
+      const dataset = this.processOlderData;
+
+      d3.select(this.$refs.chart)
+        .select('svg')
+        .remove();
+
+      const svg = d3.select(this.$refs.chart)
+        .append('svg')
+        .attr("viewBox", `0 0 ${this.width + this.margin.left + this.margin.right} ${this.height + this.margin.top + this.margin.bottom}`)
+        .append('g')
+        .attr('transform', `translate(${this.margin.left}, ${this.margin.top})`);
+
+      const x = d3.scaleBand()
+        .domain(dataset.map(d => this.parseLabels(d.key)))
+        .range([0, this.width])
+        .padding(0.1);
+
+      const y = d3.scaleLinear()
+        .domain([0, d3.max(dataset, d => d.doc_count)])
+        .nice()
+        .range([this.height, 0]);
+
+      const xAxis = svg.append('g')
+        .attr('class', 'x-axis')
+        .attr('transform', `translate(0,${this.height})`)
+        .call(d3.axisBottom(x).tickSize(0));
+
+      if (dataset.length > 8) {
+        xAxis.selectAll('text')
+          .style('text-anchor', 'end')
+          .attr('dx', '-0.8em')
+          .attr('dy', '0.15em')
+          .attr('transform', 'rotate(-45)');
+      }
+
+      const tooltip = d3.select(this.$refs.tooltip)
+
+      svg.selectAll('.bar')
+        .remove();
+
+      this.bars = svg.selectAll('.bar')
+        .data(dataset)
+        .enter().append('rect')
+        .attr('class', 'bar')
+        .attr('x', d => x(this.parseLabels(d.key)))
+        .attr('y', d => y(d.doc_count))
+        .attr('width', x.bandwidth())
+        .attr('height', d => this.height - y(d.doc_count))
+        .style("margin-left", function(d) { return "0px"; })
+        .attr('fill', '#DE6E26')
+
+      svg.append('g')
+        .attr('class', 'y-axis')
+        .call(d3.axisLeft(y).ticks(5));
+    },
+  },
+  template: `
+    <div ref="chart" style="width: 100%; position: relative;"></div>
+  `
+};
