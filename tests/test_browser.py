81329a4c762227d8199f06a997f436f567a7a562
Michael Wood <michael.wood@opendataservices.coop>
2023-10-16 18:34:26 +0100

    tests: Remove redundant backend tests
    Fix requirements as pytest-flask not yet compatible with flask 3.
    
    This leaves one very basic test which tests to see if the page loads
    without any javascript errors. Further tests are needed to test
    interactions. When we have a live backend for this this will be able to
    be added.

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -5,51 +5,43 @@ import pytest
 from flask import url_for
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-from insights.db import Publisher
+os.environ["FLASK_ENV"] = "development"
+BROWSER = os.environ.get("BROWSER", "ChromeHeadless")
 
-os.environ['FLASK_ENV'] = 'development'
-BROWSER = os.environ.get('BROWSER', 'ChromeHeadless')
-
-# Ensure the correct version of chromedriver is installed
-chromedriver_autoinstaller.install()
 
 @pytest.fixture(scope="module")
 def browser(request):
-    if BROWSER == 'ChromeHeadless':
+    if BROWSER == "ChromeHeadless":
+        # Ensure the correct version of chromedriver is installed
+        chromedriver_autoinstaller.install()
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         # uncomment this if "DevToolsActivePort" error
-        # chrome_options.add_argument("--remote-debugging-port=9222")
+        chrome_options.add_argument("--remote-debugging-port=9222")
         browser = webdriver.Chrome(options=chrome_options)
     else:
+        print("Unsupported BROWSER")
         browser = getattr(webdriver, BROWSER)()
     browser.implicitly_wait(3)
     request.addfinalizer(lambda: browser.quit())
     return browser
 
 
-@pytest.mark.usefixtures('live_server')
+@pytest.mark.usefixtures("live_server")
 def test_home(browser):
-    browser.get(url_for('data', _external=True))
-    assert 'Insights' in browser.find_element_by_tag_name('body').text
+    browser.get(url_for("data", _external=True))
+    assert "Insights" in browser.find_element(By.TAG_NAME, "body").text
 
 
-@pytest.mark.usefixtures('live_server')
+@pytest.mark.usefixtures("live_server")
 def test_javascript_errors_in_page(browser):
-
     def check_browser():
         for log in browser.get_log("browser"):
-            assert "SEVERE" not in log['level'], f"found {log}"
+            assert "SEVERE" not in log["level"], f"found {log}"
 
-    browser.get(url_for('data', _external=True))
+    browser.get(url_for("data", _external=True))
     check_browser()
-
-    data_url = url_for('data', _external=True)
-
-    for publisher in Publisher.query.filter()[:5]:
-        browser.get("%s/data?funders=%s" % (data_url, publisher.prefix))
-        browser.implicitly_wait(2)
-        check_browser()
